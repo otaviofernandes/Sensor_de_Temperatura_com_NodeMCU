0bab5454ee0c4ee33b39a009a1a087ecee42b373
Otavio de Almeida Fernandes <66568816+otaviofernandes@users.noreply.github.com>
2021-02-17 18:23:23 -0300

    Update leitura_esp8266.py

--- leitura_esp8266.py
+++ leitura_esp8266.py
@@ -2,27 +2,90 @@ import requests
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from itertools import count
+import PySimpleGUI as sg
 
+
+def testa_arquivo():
+    try:
+        arquivo = open('ipsalvo.txt', 'r')
+    except FileNotFoundError:
+        arquivo = open('ipsalvo.txt', 'w')
+        arquivo.close()
+
+
+def le_ipsalvo():
+    arquivo = open('ipsalvo.txt', 'r')
+    ipsalvo = arquivo.read()
+    arquivo.close
+    return ipsalvo
+
+
+def salva_ultimo_ip(ip):
+    arquivo = open('ipsalvo.txt', 'w')
+    arquivo.write(ip)
+    arquivo.close()
+
+
+def escala():
+    sg.theme('DefaultNoMoreNagging')
+    layout = [
+        [sg.Text('Escolha a escala para leitura de temperatura')],
+        [sg.Radio('Celsius', escala, key='c'), sg.Radio('Fahrenheit', escala, key='f')],
+        [sg.Text('')],
+        [sg.Text('Digite o endereço IP para conexão com o NodeMCU')],
+        [sg.Text('IP do último acesso -> ' + ipsalvo)],
+        [sg.Input()],
+        [sg.Button('Iniciar ou Finalizar')]
+    ]
+    return sg.Window('Escala de Temperatura', layout=layout, finalize=True)
+
+
+def atualiza_dados(tp, ip):
+    resposta = requests.get('http://'+ip+'/')
+    dados = resposta.text
+    tempo_seg, temperatura_c = dados.split()
+    tempo.append(float(tempo_seg)/60)
+    if tp =='C':
+        temperatura.append(float(temperatura_c))
+    else:
+        temperatura_c = float(temperatura_c)*9/5 +32
+        temperatura.append(temperatura_c)
+
+
+def grafico_dinamico(i):
+    atualiza_dados(tp, ip)
+    if tp == 'C':
+        plt.plot(tempo, temperatura, c='red', ls=':', label='Temp, °C')
+        plt.title('Temperatura (°C) por tempo (minutos)', color='black')
+        plt.xlabel('Tempo (minutos)')
+        plt.ylabel('Temperatura (°C)')
+    else:
+        plt.plot(tempo, temperatura, c='red', ls=':', label='Temp, °F')
+        plt.title('Temperatura (°F) por tempo (minutos)', color='black')
+        plt.xlabel('Tempo (minutos)')
+        plt.ylabel('Temperatura (°F)')
+
+# ----------------------------------------------------------------------------------
 tempo = [0]
 temperatura = [0]
 index = count()
 
-def atualiza_dados():
-    resposta = requests.get('http://***.***.*.**/') # inserir aqui o endereço ip da placa do NodeMCU.
-    dados = resposta.text
-    tempo_seg, temperatura_c = dados.split()
-    tempo.append(float(tempo_seg)/60)
-    temperatura.append(float(temperatura_c))
+testa_arquivo()
+ipsalvo = le_ipsalvo()
 
-def grafico_dinamico(i):
-    atualiza_dados()
-    plt.plot(tempo, temperatura, c='red', ls=':', label='Temp, °C')
-    plt.title('Temperatura (°C) por tempo (minutos)', color='black')
-    plt.xlabel('Tempo (minutos)')
-    plt.ylabel('Temperatura (°C)')
+janela = escala()
+janela.maximize()
 
-ani = FuncAnimation(plt.gcf(), grafico_dinamico, interval=5000)
-plt.tight_layout()
+event, values = janela.Read()
+if event == sg.WIN_CLOSED:
+    exit(69)
+
+ip = values[0]
+salva_ultimo_ip(ip)
+
+if values['c']:
+    tp = 'C'
+else:
+    tp = 'F'
+ani = FuncAnimation(plt.gcf(), grafico_dinamico, interval=1000)
 plt.show()
-
-
