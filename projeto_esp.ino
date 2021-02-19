6afa6f363262f762d1f98dab7dca68c51566dae2
Otavio de Almeida Fernandes <66568816+otaviofernandes@users.noreply.github.com>
2021-02-19 18:12:06 -0300

    Update projeto_esp.ino

--- projeto_esp.ino
+++ projeto_esp.ino
@@ -13,19 +13,22 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 int thermoDO = D4;
 int thermoCS = D5;
 int thermoCLK = D6;
-int led = D7;
+int led_on = D7;
+int led_off = D8;
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 
-const char* ssid = "********"; // insiria aqui o nome da rede wifi.
-const char* password = "********"; // insiria aqui a senha de rede.
+
+const char* ssid = "***********"; // insiria aqui o nome da rede wifi.
+const char* password = "*******"; // insiria aqui a senha de rede.
 int od = 0;
+int ct = 10;
 
 ESP8266WebServer server(80);
 
 void handleRoot() {
-  String result = String(thermocouple.readCelsius());
-  String ordem = String(od);
-  server.send(200, "text/plain", ordem + " " + result);
+String result = String(thermocouple.readCelsius());
+String ordem = String(od);
+server.send(200, "text/plain", ordem + " " + result);
 }
 
 void handleNotFound(){
@@ -35,8 +38,10 @@ void handleNotFound(){
 
 void setup(void){
 
- pinMode(led, OUTPUT);
-   //-----------------------------------------------------
+ pinMode(led_on, OUTPUT);
+ pinMode(led_off, OUTPUT);
+ 
+ //-----------------------------------------------------
  Wire.begin(D2, D1);
  lcd.init();
  lcd.setBacklight(HIGH);
@@ -48,42 +53,43 @@ void setup(void){
  //-----------------------------------------------------
 
   Serial.begin(115200);
-  //Serial.println("MAX6675 test");
-  // wait for MAX chip to stabilize
-  //delay(500);
   
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  //Serial.println("");
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  while (ct > 0){
+  Serial.print("Conectando ->");
+  delay(500);
+  ct = ct - 1;
+  Serial.println(ct);
   }
-  //Serial.println("");
+  if (WiFi.status() == WL_CONNECTED){
   Serial.print("Connected to ");
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   
-
   if (MDNS.begin("esp8266")) {
-    Serial.println("MDNS responder started");
+  Serial.println("MDNS responder started");
   }
 
   server.on("/", handleRoot);
+
   server.onNotFound(handleNotFound);
+
   server.begin();
   Serial.println("HTTP server started");
+  }
 }
 
 void loop(void){
-  digitalWrite(led, HIGH);
+  if(WiFi.status() == WL_CONNECTED){ 
+  Serial.println("ONLINE");
+  digitalWrite(led_on, HIGH);
   delay(100);
-  digitalWrite(led, LOW);
+  digitalWrite(led_on, LOW);
   server.handleClient();
-
+  
  //-----------------------------------------------------
   lcd.setBacklight(HIGH);
   lcd.setCursor(0,0);
@@ -99,5 +105,30 @@ void loop(void){
   lcd.print("Temp:  " + rf + "  F");
   delay(900);
  //-----------------------------------------------------
+
+  }
+  else{
+  Serial.println("OFFLINE");
+  digitalWrite(led_off, HIGH);
+  delay(100);
+  digitalWrite(led_off, LOW);
+  
+ //-----------------------------------------------------
+  lcd.setBacklight(HIGH);
+  lcd.setCursor(0,0);
+  lcd.print("----OFF-LINE----");
+  lcd.setCursor(0,1);
+  String rc = String(thermocouple.readCelsius());
+  String rf = String(thermocouple.readFahrenheit());
+  lcd.print("Temp:  " + rc + "  C");
+  delay(1000);
+  lcd.setCursor(0,0);
+  lcd.print("----OFF-LINE----");
+  lcd.setCursor(0,1);
+  lcd.print("Temp:  " + rf + "  F");
+  delay(900);
+ //-----------------------------------------------------
+
+  }
   od = od + 2;
 }
